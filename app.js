ccd5682fdd586a7c2830ba8d7c40519759cc9233
Sebastian <sebastianglg64@gmail.com>
2024-05-22 17:02:11 -0500

    91 - servir sitio web completo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 
 // definiendo variables globales
 dotenv.config();
@@ -9,21 +10,28 @@ const app = express();
 
 
 // servir contenido estático - referencia al public/index.html
-app.use( express.static('public') );
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use( express.static('public') );
 
 
-
-app.get('/', function (req, res) {
-  res.send('Sin parametros');
-})
-
-app.get('/hola-mundo', function (req, res) {
-  res.send('Ruta desde hola-mundo'); // de prueba
+app.get('/index', (req, res) => {
+  res.sendFile( path.join(__dirname + '/public/index.html'));
 });
 
-app.get('*', function (req, res) {
+
+app.get('/elements', (req, res) => {
+  res.sendFile( path.join(__dirname + '/public/elements.html'));
+});
+
+
+app.get('/generic', (req, res) => {
+  res.sendFile( path.join(__dirname + '/public/generic.html'));
+});
+
+
+app.get('*', (req, res) => {
   // res.send('Page 404 || Nots found');
-  res.sendFile( __dirname + '/public/404.html'); // dirname apunta al url principal
+  res.sendFile( path.join(__dirname + '/public/404.html')); // dirname apunta al url principal
 });
 
 
